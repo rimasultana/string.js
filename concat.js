395a1bcc4d1a11e4b3f7d4a3d7faac785dce7106
rimasultana <rimasultana20225@gmail.com>
2024-09-03 11:13:34 +0600

    string

---
+++ concat.js
@@ -0,0 +1,11 @@
+const last = 'Sultana';
+const first = 'Rima';
+
+// const FullName = first + ' ' + last;
+// console.log(FullName);
+
+const FullName2 = first.concat(' ').concat(last) ;
+console.log(FullName2);
+
+console.log(last.includes('a'));
+console.log(first.includes('n'));
---
+++ slice.js
@@ -0,0 +1,19 @@
+// const address = "RimaSultan";
+// const part = address.slice(4, 7);
+// console.log(part);
+
+// const sentence = "I am a good and hardworking person";
+
+// console.log(sentence.split(" "));
+
+// console.log(sentence.split('a'));
+
+// alada upadan banano
+
+const friendStr = 'Rahim, Kahim,Rkiuja';
+const frients = friendStr.split(',')
+console.log(frients);
+
+const realFrient = ['Rahim','Hahjui', 'jshujfh','sjf'];
+console.log(realFrient.join());
+console.log(realFrient.join('|'));
