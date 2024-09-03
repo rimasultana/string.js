b8fb2e364b50afed3eab775bd81d9e4ae098169e
rimasultana <rimasultana20225@gmail.com>
2024-09-03 20:56:41 +0600

    string

---
+++ reverse.js
@@ -0,0 +1,12 @@
+const sentence = 'I am learning Web Dev';
+
+let reverse = '';
+// for(const letter of sentence){
+//     // console.log(letter);
+//     reverse = letter + reverse;
+// }
+// console.log(reverse);
+
+for (let i = 0; i < sentence.length; i++){
+    console.log(sentence[i]);
+}
---
+++ stringStr.js
@@ -0,0 +1,25 @@
+// looping technique
+// for loop
+// while loop
+// do-while
+// for of ==> array loop korar jonno
+// for in ==> object loop korar jonno
+
+// const friend = ['mark', 'bill', 'rima', 'fkjdi'];
+
+// for(let i = 0; i <friend.length; i++){
+//     console.log(i);
+//     console.log(friend[i]);
+// }
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(numbers);
+numbers.reverse();
+console.log(numbers);
+
+
+
+
+
+
+
+
